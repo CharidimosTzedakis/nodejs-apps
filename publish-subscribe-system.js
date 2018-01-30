71228744df0066e5b713e9cc3ae6ac5fc2b06052
Tzedakis Charidimos <harris.jedakis@gmail.com>
2018-01-30 06:57:31 +0200

    added last book functionalities and work on startup command

--- publish-subscribe-system.js
+++ publish-subscribe-system.js
@@ -24,20 +24,43 @@ channel.on('leave', function(id){
   channel.emit('broadcast', id, `${id} has left the chatroom.\n`);
 });
 
+//----shutdown command-----//
 //prevent chat without shutting dowm the server
 channel.on('shutdown', () => {
-  channel.emit('broadcast', '', 'The server has shut down.\n');
   channel.removeAllListeners('broadcast');
+  channel.emit('broadcast', '', 'This room has shut down.\n');
 });
 
+//----startup command-----//
+//prevent chat without shutting dowm the server
+channel.on('start', () => {
+  //for ()
+  //this.on('broadcast', this.subscriptions[id]);
+
+  channel.emit('broadcast', '', 'The server is starting.\n');
+});
+
+
+//Show welcome message with number of listeners
+channel.on('join', function(id, client) {
+  const welcome = `Welcome!\r\nGuests online: ${this.listeners('broadcast').length}
+                  `;
+  client.write(`${welcome}\r\n`);
+});
+
+//set max number of listeners
+channel.setMaxListeners(50);
 
 const server = net.createServer(client => {
   const id = `${client.remoteAddress}:${client.remotePort}`;
   channel.emit('join', id, client);
   client.on('data', data => {
       data = data.toString();
-      if (data === 'shutdown\r\n') {
+      if (data === '/shutdown\r\n') {
         channel.emit('shutdown');
+      }
+      if (data === '/start\r\n') {
+        channel.emit('start');
       }
       channel.emit('broadcast', id, data);
   });
