18b45b2beace1236f3f3c5f99285477ba469251a
Tzedakis Charidimos <harris.jedakis@gmail.com>
2018-02-05 00:16:18 +0200

    commands independent of CR/LF, independent of number of tcp packets

--- publish-subscribe-system.js
+++ publish-subscribe-system.js
@@ -21,23 +21,23 @@ channel.on('join', function(id, client) {
 //a specific client
 channel.on('leave', function(id){
   channel.removeListener('broadcast', this.subscriptions[id]);
-  channel.emit('broadcast', id, `${id} has left the chatroom.\n`);
+  channel.emit('broadcast', id, `${id} has left the chatroom.\r\n`);
 });
 
 //----shutdown command-----//
 //prevent chat without shutting dowm the server
 channel.on('shutdown', () => {
-  channel.emit('broadcast', '', 'This room has shut down.\n');
+  channel.emit('broadcast', '', 'This room has shut down.\r\n');
   channel.removeAllListeners('broadcast');
 });
 
 //----startup command-----//
 //prevent chat without shutting dowm the server
 channel.on('start', () => {
-  for (var id in this.subscriptions){
-    this.on('broadcast', this.subscriptions[id]);
+  for (var id in channel.subscriptions){
+    channel.on('broadcast', channel.subscriptions[id]);
   }
-  channel.emit('broadcast', '', 'The server has started.\n');
+  channel.emit('broadcast', '', 'The server has started.\r\n');
 });
 
 
@@ -54,20 +54,21 @@ channel.setMaxListeners(50);
 const server = net.createServer(client => {
   const id = `${client.remoteAddress}:${client.remotePort}`;
   channel.emit('join', id, client);
+
   client.on('data', data => {
       data = data.toString();
-      console.log(data);
-      if (data === '/shutdown\r\n') {
-        channel.emit('shutdown');
+      if (data.startsWith('/shutdown')){
+         channel.emit('shutdown');
       }
-      if (data === '/start\r\n') {
-        channel.emit('start');
+      if (data.startsWith('/start')){
+         channel.emit('start');
       }
       channel.emit('broadcast', id, data);
   });
+
   client.on('close', () => {
     channel.emit('leave', id);
   });
 });
-
+//'192.168.1.5'
 server.listen(8888);
